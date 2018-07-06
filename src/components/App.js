aa35e1118a8df7d737332819d702860e00bed451
Michael DuBose <mhdubose@gmail.com>
2018-07-06 16:27:15 -0700

    Skeleton app with Banner and Article List

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import Header from './Header';
+import Home from './Home';
+import * as React from "react";
+import {connect} from 'react-redux';
+
+const mapStateToProps = state => ({
+    appName: state.appName
+});
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <Header appName={this.props.appName}/>
+                <Home/>
+            </div>
+        );
+    }
+}
+
+export default connect(mapStateToProps, () => ({}))(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
+import App from './components/App';
+import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {createStore} from 'redux';
 
-class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
-  }
-}
+const defaultState = {
+    appName: 'conduit',
+    articles: null
+};
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case 'TOGGLE':
+            return {...state, checked: !state.checked};
+        default:
+            return state;
+    }
+};
+const store = createStore(reducer);
 
 ReactDOM.render((
-  <App />
+    <Provider store={store}>
+        <App/>
+    </Provider>
 ), document.getElementById('root'));
